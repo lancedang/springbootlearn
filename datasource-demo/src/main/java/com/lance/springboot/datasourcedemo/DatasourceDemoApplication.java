1e5173a0b12b332fa9a022992d1d9b940ffd5afa
qiankai <qiankai07@meituan.com>
2020-03-01 18:32:04 +0800

    用tomcat-jdbc自带的org.apache.tomcat.jdbc.pool.DataSource做演示

--- datasource-demo/src/main/java/com/lance/springboot/datasourcedemo/DatasourceDemoApplication.java
+++ datasource-demo/src/main/java/com/lance/springboot/datasourcedemo/DatasourceDemoApplication.java
@@ -2,16 +2,19 @@ package com.lance.springboot.datasourcedemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import javax.sql.DataSource;
 
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
 public class DatasourceDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DatasourceDemoApplication.class, args);
-        System.out.println();
+        //System.out.println("hi");
     }
 
 }
--- datasource-demo/src/test/java/com/lance/springboot/datasourcedemo/DatasourceDemoApplicationTests.java
+++ datasource-demo/src/test/java/com/lance/springboot/datasourcedemo/DatasourceDemoApplicationTests.java
@@ -1,16 +1,47 @@
 package com.lance.springboot.datasourcedemo;
 
+import com.lance.springboot.datasourcedemo.entity.Student;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = DatasourceDemoApplication.class)
 public class DatasourceDemoApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws SQLException {
+        System.out.println(dataSource);
+        Connection connection = dataSource.getConnection();
+
+        List<Student> students = new ArrayList<Student>(16);
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from student");
+        Student student = null;
+        while (resultSet.next()){
+            student = new Student();
+            student.setId(resultSet.getInt("id"));
+            student.setName(resultSet.getString("name"));
+            student.setPassword(resultSet.getString("password"));
+            student.setAge(resultSet.getInt("age"));
+            students.add(student);
+        }
+        System.out.println(students);
+
     }
 
 }
