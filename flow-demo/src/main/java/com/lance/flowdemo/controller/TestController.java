95ec64b5cbf2408a22817101d8aa22b89f5fece8
qiankai1 <qiankai1@xiaomi.com>
2022-03-22 20:44:12 +0800

    add @ControllerAdvice和 ResponseBodyAdvice

---
+++ flow-demo/src/main/java/com/lance/flowdemo/controller/TestController.java
@@ -0,0 +1,53 @@
+package com.lance.flowdemo.controller;
+
+import com.lance.flowdemo.entity.MyTestBean;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api")
+public class TestController {
+
+    @RequestMapping(value = "/test", produces = MediaType.APPLICATION_XML_VALUE)
+    public MyTestBean test() {
+        MyTestBean bean = new MyTestBean();
+        bean.setAddr("shanghai");
+        bean.setName("qiankai");
+        return bean;
+    }
+
+
+    @RequestMapping(value = "/test2", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public MyTestBean test2() {
+        MyTestBean bean = new MyTestBean();
+        bean.setAddr("上海");
+        bean.setName("qiankai");
+        return bean;
+    }
+
+    @RequestMapping(value = "/test3")
+    public MyTestBean test3(@RequestParam String name) {
+        MyTestBean bean = new MyTestBean();
+        bean.setAddr(name);
+        bean.setName("qiankai");
+        return bean;
+    }
+
+    @RequestMapping(value = "/test4", method = RequestMethod.POST)
+    public MyTestBean test4(@RequestBody MyTestBean testBean) {
+
+        testBean.setAddr("shanghai");
+        testBean.setName("qiankai");
+
+        return testBean;
+    }
+
+    @RequestMapping(value = "/test5", method = RequestMethod.POST)
+    public MyTestBean test5() {
+
+        if (true) {
+            throw new RuntimeException("test errro");
+        }
+        return null;
+    }
+}
